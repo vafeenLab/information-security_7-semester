ce0391746096e44c11181422bd852cc89d564421
A <666av6@gmail.com>
2025-09-13 21:11:24 +0300

    Add docs for MatrixCryptographer.kt

--- src/main/kotlin/lab1/MatrixCryptographer.kt
+++ src/main/kotlin/lab1/MatrixCryptographer.kt
@@ -2,6 +2,11 @@ package lab1
 
 private typealias Matrix = Array<CharArray>
 
+/**
+ * Преобразует матрицу символов в строковое представление с рамкой из дефисов.
+ * @receiver Исходная матрица символов.
+ * @return Строка с отображением матрицы.
+ */
 fun Array<CharArray>.toMatrixString(): String {
     val sizeOfBorder = 20
 
@@ -11,22 +16,41 @@ fun Array<CharArray>.toMatrixString(): String {
             "-".repeat(sizeOfBorder) + "\n"
 }
 
+/**
+ * Класс, реализующий шифрование и дешифрование текста с помощью ключей для строк и колонок.
+ * @property keysForRow Список ключей для перестановки символов по строкам.
+ * @property keysForColumn Список ключей для перестановки символов по столбцам.
+ */
 internal class MatrixCryptographer(
     val keysForRow: List<Int>,
     val keysForColumn: List<Int>,
 ) {
+    /**
+     * Шифрует входящую строку, преобразуя её в матрицу и применяя кодирование строк и столбцов.
+     * @param str Исходная строка для кодирования.
+     * @return Закодированная матрица символов.
+     */
     fun encode(str: String): Matrix =
         input(str)
             .encodeRows()
             .encodeColumns()
 
-
+    /**
+     * Дешифрует матрицу, выполняя транспонирование, декодирование строк и столбцов.
+     * @param matrix Матрица для декодирования.
+     * @return Раскодированная матрица символов.
+     */
     fun decode(matrix: Matrix) =
         matrix
             .transpose()
             .decodeRows()
             .decodeColumns()
 
+    /**
+     * Декодирует строки матрицы на основе ключей для колонок.
+     * @receiver Матрица для декодирования.
+     * @return Новая матрица с декодированными строками.
+     */
     fun Matrix.decodeRows(): Matrix {
         val result = this.deepClone()
         this.forEachIndexed { strIndex, str ->
@@ -37,6 +61,11 @@ internal class MatrixCryptographer(
         return result
     }
 
+    /**
+     * Декодирует столбцы матрицы на основе ключей для строк.
+     * @receiver Матрица для декодирования.
+     * @return Новая матрица с декодированными столбцами.
+     */
     fun Matrix.decodeColumns(): Matrix {
         val result = this.deepClone()
         keysForRow.forEachIndexed { indexOfIndex, index ->
@@ -45,6 +74,12 @@ internal class MatrixCryptographer(
         return result
     }
 
+    /**
+     * Преобразует входящую строку в матрицу символов фиксированного размера.
+     * @param str Входящая строка для преобразования.
+     * @throws Exception Если длина строки не соответствует размеру матрицы.
+     * @return Сформированная матрица символов.
+     */
     private fun input(str: String): Matrix {
         if (str.length != keysForRow.size * keysForColumn.size) throw Exception("this str cannot be encoded")
         val matrix = Array(keysForRow.size) { CharArray(keysForColumn.size) { 'x' } }
@@ -58,6 +93,11 @@ internal class MatrixCryptographer(
         return matrix
     }
 
+    /**
+     * Кодирует строки матрицы, переставляя символы согласно ключам для строк.
+     * @receiver Матрица для кодирования.
+     * @return Новая матрица с закодированными строками.
+     */
     fun Matrix.encodeRows(): Matrix {
         val result = this.deepClone()
         this.forEachIndexed { strIndex, str ->
@@ -68,6 +108,11 @@ internal class MatrixCryptographer(
         return result
     }
 
+    /**
+     * Кодирует столбцы матрицы, переставляя строки согласно ключам для колонок.
+     * @receiver Матрица для кодирования.
+     * @return Новая матрица с закодированными столбцами.
+     */
     fun Matrix.encodeColumns(): Matrix {
         val result = this.deepClone()
         keysForColumn.forEachIndexed { indexOfIndex, index ->
@@ -76,6 +121,11 @@ internal class MatrixCryptographer(
         return result
     }
 
+    /**
+     * Выполняет транспонирование матрицы.
+     * @receiver Матрица для транспонирования.
+     * @return Новая транспонированная матрица.
+     */
     fun Matrix.transpose(): Matrix {
         val result = this.deepClone()
         this.forEachIndexed { indexStr, str ->
@@ -86,6 +136,11 @@ internal class MatrixCryptographer(
         return result
     }
 
+    /**
+     * Преобразует ключ (начинающийся с 1) в индекс массива (начинающийся с 0).
+     * @receiver Ключ в системе отсчёта от 1.
+     * @return Соответствующий индекс с отсчётом от 0.
+     */
     private fun Int.keyToIndex(): Int = this - 1
 
     /**
